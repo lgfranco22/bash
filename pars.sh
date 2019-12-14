0ec5ed57f71d3e8b6919876873b5f08fe706a14e
lgfranco22 <lgfranco22@live.com>
2019-12-14 12:34:10 +0000

    script para parsing em sites

---
+++ pars.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+clear
+if [ "$1" == "" ]
+then
+echo -e " \033[1;35m#######################################\033[0m"
+echo -e " \033[5;32m Buscando Hosts\033[0m"
+echo -e " \033[0;32m Desec Security - Luiz Gonzaga Franco\033[0m"
+echo -e " \033[0;32m $0 www.alvo.com.br\033[0m"
+echo -e " \033[1;35m#######################################\033[0m"
+else
+echo -e "\033[1;35m#####################################\033[0m"
+echo -e "\033[5;32mBuscando Hosts....\033[0m"
+echo -e "\033[1;35m#####################################\033[0m "
+wget -q "$1"
+grep href index.html | cut -d "/" -f3 | grep "\." | cut -d '"' -f1 | sort -u | grep -v "return"  | grep -v "<a" | grep -v "<li" | grep -v "#" | grep ".gif" > lista
+cat lista
+echo -e "\033[1;35m#####################################\033[0m"
+echo -e "\033[5;32mResolvendo Hosts....\033[0m"
+echo -e "\033[1;35m#####################################\033[0m"
+for url in $(cat lista);do host $url;done | grep "has address"
+rm lista
+rm index.html
+fi
